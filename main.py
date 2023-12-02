de5d24b751b939db989ea45562df169bece3c88f
Daniele Giaquinto <exSnake@hotmail.it>
2023-12-03 00:53:56 +0100

    fix log propagation

--- main.py
+++ main.py
@@ -19,30 +19,35 @@ PAPERTRAIL_HOST = os.getenv('PAPERTRAIL_HOST')
 PAPERTRAIL_PORT = int(os.getenv('PAPERTRAIL_PORT'))
 
 # Configura il logger per inviare i log a Papertrail
+# Definizione di una classe di gestore personalizzato
+class ColoredFormatterHandler(logging.StreamHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Crea i formatter per i diversi livelli di log
+        self.formatter_info = logging.Formatter('[\x1b[34m%(levelname)s\x1b[0m] %(message)s')  # Info in blu
+        self.formatter_warn = logging.Formatter('[\x1b[33m%(levelname)s\x1b[0m] %(message)s')  # Warn in giallo
+        self.formatter_error = logging.Formatter('[\x1b[31m%(levelname)s\x1b[0m] %(message)s')  # Error in rosso
+
+    def emit(self, record):
+        if record.levelno == logging.INFO:
+            self.setFormatter(self.formatter_info)
+        elif record.levelno == logging.WARNING:
+            self.setFormatter(self.formatter_warn)
+        elif record.levelno >= logging.ERROR:
+            self.setFormatter(self.formatter_error)
+
+        return super().emit(record)
+
+# Configura il logger principale
 logger = logging.getLogger('speedtest')
 logger.setLevel(logging.INFO)
 
-# Handler per i livelli di log INFO
-handler = logging.handlers.SysLogHandler(address=(PAPERTRAIL_HOST , PAPERTRAIL_PORT))
-formatter = logging.Formatter('[\x1b[34m%(levelname)s\x1b[0m] %(message)s')  # Info in blu
-handler.setFormatter(formatter)
+# Crea un'istanza del gestore personalizzato
+handler = ColoredFormatterHandler()
+
+# Aggiungi il gestore al logger principale
 logger.addHandler(handler)
-
-# Handler per i livelli di log WARN
-handler_warn_error = logging.handlers.SysLogHandler(address=(PAPERTRAIL_HOST , PAPERTRAIL_PORT))
-formatter_warn_error = logging.Formatter('[\x1b[33m%(levelname)s\x1b[0m] %(message)s')  # Warn in giallo
-handler_warn_error.setFormatter(formatter_warn_error)
-handler_warn_error.setLevel(logging.WARNING)
-logger.addHandler(handler_warn_error)
-
-# Handler per i livelli di log ERROR
-handler_error = logging.handlers.SysLogHandler(address=(PAPERTRAIL_HOST , PAPERTRAIL_PORT))
-formatter_error = logging.Formatter('[\x1b[31m%(levelname)s\x1b[0m] %(message)s')  # Error in rosso
-handler_error.setFormatter(formatter_error)
-handler_error.setLevel(logging.ERROR)
-logger.addHandler(handler_error)
-
-logger.propagate = False
 
 async def run_speed_test():
     try:
@@ -69,11 +74,10 @@ async def main():
     while download_speed is None and retry < 3:
         retry += 1
         logger.warning(f"Errore durante lo speed test. Ritento {retry}...")
-        await asyncio.sleep(random.randint(65, 125))
+        await asyncio.sleep(random.randint(125, 300))
         download_speed = await run_speed_test()
     if download_speed is None:
         logger.error("Errore durante lo speed test. Controlla la connessione.")
-        await send_telegram_notification("Errore durante lo speed test. Controlla la connessione.")
     elif download_speed <= 100:
         logger.info(f"La velocità di download è inferiore o uguale a 100 Mbps: {download_speed:.2f} Mbps, controllare il cavo di rete sullo switch.")
         await send_telegram_notification(f"La velocità di download è inferiore o uguale a 100 Mbps: {download_speed:.2f} Mbps")
